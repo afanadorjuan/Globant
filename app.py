eaab9e096292c5781d5b2d87f55463512e433d24
Juan Afanador <j.afanador@javeriana.edu.co>
2023-08-02 12:31:59 -0500

    /insert_batch and /get_data added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import os
 import pandas as pd
 import sqlite3
+
 app = Flask(__name__)
 
 
@@ -30,6 +31,49 @@ def upload_csv():
 
     except Exception as e:
         return jsonify({'error': 'An error occurred while processing the CSV file.', 'details': str(e)}), 500
+
+@app.route('/insert_batch', methods=['POST'])
+def insert_batch():
+    try:
+        # Obtener los datos enviados en la solicitud
+        data = request.json
+
+        # Validar que se envió un lote de transacciones
+        if not data or not isinstance(data, list):
+            return jsonify({'error': 'Invalid data. Please send a batch of transactions as a list of dictionaries.'}), 400
+
+        # Guardar el lote de transacciones en la base de datos SQLite
+        conn = sqlite3.connect(os.path.join(db_folder, 'database.db'))
+        df = pd.DataFrame(data)
+        df.to_sql('data_table', conn, if_exists='append', index=False)
+
+        return jsonify({'message': 'Batch of transactions inserted successfully!'})
+
+    except Exception as e:
+        return jsonify({'error': 'An error occurred while processing the batch of transactions.', 'details': str(e)}), 500
+
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    try:
+        # Establecer conexión con la base de datos SQLite
+        conn = sqlite3.connect(os.path.join(db_folder, 'database.db'))
+
+        # Ejecutar una consulta SQL para obtener todos los datos de la tabla data_table
+        cursor = conn.execute('SELECT * FROM data_table')
+
+        # Obtener los resultados de la consulta
+        data = cursor.fetchall()
+
+        # Cerrar la conexión con la base de datos
+        conn.close()
+
+        # Devolver los datos en formato JSON
+        return jsonify(data)
+
+    except Exception as e:
+        return jsonify({'error': 'An error occurred while querying the database.', 'details': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
